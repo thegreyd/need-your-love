10633c9ef9f8a8cbc2a0baa095613fdf0317e246
Kaustubh <sid.sharma0@gmail.com>
2013-08-22 13:53:19 +0530

    added program to print digitPrimes in python

---
+++ digitPrimes.py
@@ -0,0 +1,37 @@
+def checkPrime(n):
+	for x in range(2,int(n**0.5)+1):
+		if not n%x:
+			return False
+	return True
+def generatePrimes(up,low):
+	plist=[]
+	for x in range(up,low+1):
+		if checkPrime(x):
+			plist.append(x)
+	return plist
+def digitSum(n):
+	sum=0
+	while n:
+		sum+=n%10
+		n//=10
+	return sum
+def generatedigitPrimes(up,low):
+	dplist=[]
+	primeslist=generatePrimes(up,low)
+	for x in primeslist:
+		if checkPrime(digitSum(x)):
+			dplist.append(x)
+	return dplist
+
+def main():
+	s=input().split(" ")
+	uplim=int(s[0])
+	lowlim=int(s[1])
+	index=int(s[2])
+	dprimes=generatedigitPrimes(uplim,lowlim)
+	print(dprimes)
+	if index<len(dprimes):
+		print(dprimes[index])
+	else:
+		print("No number at this index")
+main()
